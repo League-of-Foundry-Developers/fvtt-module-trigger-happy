ce2c2cd424f0045caf7c852f1dc55fcc7e10281c
Tim Posney <tposney@gmail.com>
2020-04-03 22:32:11 +1100

    couple of bug fixes added triggers when tokens move onto a trigger token

--- trigger.js
+++ trigger.js
@@ -14,6 +14,8 @@ class TriggerHappy {
         Hooks.on('hoverToken', this._onHoverToken.bind(this));
         Hooks.on('updateJournalEntry', this._onUpdateJournal.bind(this));
         Hooks.on('deleteJournalEntry', this._onUpdateJournal.bind(this));
+        Hooks.on("preUpdateToken", this._onEndUpdate.bind(this));
+
         this.triggers = [];
         this._journalId = null;
     }
@@ -28,6 +30,7 @@ class TriggerHappy {
     _parseJournal() {
         this.triggers = []
         const journal = this.journal;
+        console.log("parse jounral journal is ", this.journal)
         if (!journal) return;
         this._journalId = journal.id;
         const triggerLines = journal.data.content.split("</p>");
@@ -76,7 +79,7 @@ class TriggerHappy {
                 } else if (effect.entity === "RollTable") {
                     await effect.draw();
                 } else if (effect.entity === "ChatMessage") {
-                    await ChatMessage.create(effect.data);
+                    await ChatMessage.create(duplicate(effect.data));
                 } else if (effect.constructor.name === "Token") {
                     const token = canvas.tokens.placeables.find(t => t.name === effect.name)
                     if (token)
@@ -105,9 +108,29 @@ class TriggerHappy {
         this._executeTriggers(triggers);
     }
 
-    _onUpdateJournal(journal) {
-        if (journal.id === this._journalId || journal.name === this.journalName)
+    _onUpdateJournal(journal, update) {
+        if (update._id === this._journalId || journal.name === this.journalName)
             this._parseJournal();
+    }
+
+    _onEndUpdate(scene, userId, update) {
+        if (update["x"] === undefined && update["y"] === undefined) return true;
+        let sceneTokens = scene.data.tokens;
+        let token = sceneTokens.find(t=>t._id === update._id);
+        if (token.hidden) return true; // hidden tokens don't trigger the trigger
+        let tokenNames = sceneTokens.map(t=>t.name); 
+        const triggers = this.triggers.filter(trigger => {
+            if (trigger.trigger.constructor.name !== "Token" || !tokenNames.includes(trigger.trigger.data.name) || trigger.trigger.data.name === token.name)
+                return false;
+            // let target = canvas.tokens.get(trigger.trigger.data._id);
+            let target = sceneTokens.find(token => token.name === trigger.trigger.data.name);
+            let tokenx = (update["x"] || token.x) + token.width * scene.data.grid / 2;
+            let tokeny = (update["y"] || token.y) + token.height * scene.data.grid / 2;
+            return (target.x <= tokenx ) && (target.x + (target.width * scene.data.grid) >= tokenx)
+                && (target.y <= tokeny ) && (target.y + (target.height * scene.data.grid) >= tokeny);
+
+        });
+        return this._executeTriggers(triggers);
     }
 }
 
